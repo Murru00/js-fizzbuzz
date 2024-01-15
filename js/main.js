356bd518f6190d2a7fe210a29cadef3ed19ae096
Murru00 <marcomurruolbia@gmail.com>
2024-01-15 01:03:55 +0100

    add numb-gen

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
 const cardsRow = document.getElementById('cards-row');
 
+// Stampo numeri da 1 a 100 con FOR
+
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
-    cardsRow.innerHTML += `
-        <div class="col-3">
-            <div class="card" >
-                <h3 class="text-center"> ${i}</h3>
-            </div>
-        </div>`;
+    let result;
+
+    //Genero un multiplo di 15 e stampo "fizzbuzz"
+    if (i % 15 == 0) {
+
+        result = 'fizzbuzz';
+        const box = `<div class="fizzbuzz card d-flex bg-warning "> ${result}</div> `;
+        cardsRow.innerHTML += box;
+    }
+
+    // Genero un multiplo di 5 e stampo "buzz"
+    else if (i % 5 == 0) {
+        result = 'buzz';
+        const box = `<div class=" buzz card d-flex"> ${result}</div> `;
+        cardsRow.innerHTML += box;
+
+    } // Genero un multiplo di 3 e stampo "fizz"
+    else if (i % 3 == 0) {
+        result = 'fizz';
+        const box = `<div class="fizz card d-flex"> ${result}</div> `;
+        cardsRow.innerHTML += box;
+
+    } else {
+        result = i;
+        const box = `<div class="num card d-flex"> ${result}</div> `;
+        cardsRow.innerHTML += box;
+    }
+
 }
